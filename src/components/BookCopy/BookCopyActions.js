016c81b8c955f51580c15592e2d815eae9752baf
Adrian Pionka <adrian@pionka.com>
2020-10-22 12:09:41 +0200

    M06L28

--- src/components/BookCopy/BookCopyActions.js
+++ src/components/BookCopy/BookCopyActions.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Stack } from "@chakra-ui/core";
 import BorrowButton from "./BorrowButton";
 import ReturnButton from "./ReturnButton";
+import { useAuth } from "../AuthProvider";
 
 export default function BookCopyActions({ bookCopy, ...remainingProps }) {
-  const canBeReturned = !!bookCopy.borrower;
-  const canBeBorrowed = !bookCopy.borrower;
+  const { currentUser } = useAuth();
+  const canBeReturned = !!bookCopy.borrower && currentUser && currentUser.id == bookCopy.borrower.id;
+  const canBeBorrowed = !bookCopy.borrower && currentUser;
 
   return (
     <Stack {...remainingProps}>
