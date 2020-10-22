c03fdc13c44abcda3af40f63fb6539f7ed50db0e
Adrian Pionka <adrian@pionka.com>
2020-10-22 12:04:25 +0200

    m06l27 - homework

--- src/components/AdminActions.js
+++ src/components/AdminActions.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Stack } from "@chakra-ui/core";
+import { useAuth } from "./AuthProvider";
 
 export default function AdminActions({ children, ...remainingProps }) {
-    if (!children) {
+    const { currentUser } = useAuth();
+    if (!children || !currentUser || !currentUser.isAdmin) {
         return null;
     }
     return (
