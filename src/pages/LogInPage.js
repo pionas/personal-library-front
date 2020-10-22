7b6a2c578b76fe6bd62ed9649193e28287ddb184
Adrian Pionka <adrian@pionka.com>
2020-10-22 11:32:37 +0200

    m06l26 - homework

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -3,15 +3,28 @@ import { gql, useMutation } from "@apollo/client";
 import LogInForm from "../components/LogInForm";
 import { useToast } from "../components/Toast";
 import { useAuth, cleanAuthToken } from "../components/AuthProvider";
-
+import { BOOK_COPY_FIELDS_FRAGMENT } from "../components/BookCopy/fragments";
+import { CURRENT_USER_DETAILS_FIELDS_FRAGMENT } from "../components/CurrentUserDetails";
+import { GET_CURRENT_USER_QUERY } from "./CurrentUserDetailsPage";
 const LOG_IN_MUTATION = gql`
   mutation LogIn($credential: LogInInput!) {
     logIn(input: $credential) {
       success
       message
       token
+      currentUser {
+        ...currentUserDetailsFields
+        ownedBookCopies {
+          ...bookCopyFields
+        }
+        borrowedBookCopies {
+          ...bookCopyFields
+        }
+      }
     }
   }
+  ${CURRENT_USER_DETAILS_FIELDS_FRAGMENT}
+  ${BOOK_COPY_FIELDS_FRAGMENT}
 `;
 export default function LogInPage() {
     const { authorize } = useAuth();
@@ -27,6 +40,20 @@ export default function LogInPage() {
                 authorize(token);
             }
         },
+        update: (cache, { data: { logIn } }) => {
+            if (!logIn || !logIn.currentUser) {
+                return;
+            }
+            try {
+                cache.writeQuery({
+                    query: GET_CURRENT_USER_QUERY,
+                    data: {
+                        currentUser: logIn.currentUser
+                    }
+                });
+            } catch (error) { }
+
+        }
     });
     return (
         <LogInForm
